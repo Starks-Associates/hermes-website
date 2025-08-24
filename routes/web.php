ce8df7785a7f4ca79f875d713a97787b6a614adf
Phillip-Hope Kọláwọlé Òjó <philliphopeojo@gmail.com>
2025-08-24 10:45:20 +0100

    feat(website): removed laravel completely, and deployed only next.js

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\PagesController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [PagesController::class, 'index']);
-Route::get('/services', [PagesController::class, 'services']);
-Route::get('/process', [PagesController::class, 'process']);
--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-    ],
-};
