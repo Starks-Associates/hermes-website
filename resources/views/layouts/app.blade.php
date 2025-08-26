d1a0558aaa6a57be3ce718aae9bee8aaafac7d8f
Phillip-Hope Kọláwọlé Òjó <philliphopeojo@gmail.com>
2025-08-26 14:29:26 +0100

    fix layout not showing styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 			<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700,900" rel="stylesheet" />
 
 			<!-- Styles / Scripts -->
-			@vite(['resources/css/app.css'])
+			@vite(['resources/css/app.css', 'resources/js/app.js'])
 			<script src="//unpkg.com/alpinejs" defer></script>
 
 	</head>
